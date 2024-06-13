91d1c025b25af79c60aa81b517dc4ebdb7ccedad
Michael Eaton <lammara96@gmail.com>
2024-06-13 12:02:03 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,17 @@
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  connection: {
+    client: 'postgres', 
+  connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapidb'),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+      },
+      debug: false,
   },
 });
