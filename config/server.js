6256ec12d509b822d2862aab537a080b5f029dbe
Michael Eaton <lammara96@gmail.com>
2024-06-23 19:03:32 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://peaceland-game.com'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  ssl: {
+    enabled: true,
+    key: env('SSL_KEY_PATH', '/etc/letsencrypt/live/peaceland-game.com/privkey.pem'),
+    cert: env('SSL_CERT_PATH', '/etc/letsencrypt/live/peaceland-game.com/fullchain.pem'),
   },
 });
